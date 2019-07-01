bcbe70c4cdb58cc0abdbbb6a41654132f75f89a1
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2019-07-01 18:44:04 +0300

    Add experiments results, Add plot model, Remove probability voting and inferencing

--- vote.py
+++ vote.py
@@ -8,7 +8,6 @@ from os.path import join
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('-in', '--folder-path')
-  parser.add_argument('--vote-type', default='binary', choices=['binary', 'probability'])
   args = parser.parse_args()
 
   files = []
@@ -16,7 +15,7 @@ if __name__ == '__main__':
     for file in filenames:
       if 'csv' in file:
         files.append(file)
-  
+
   predictions = dict()
   for file in files:
     with open(join(args.folder_path, file), 'r') as f:
@@ -25,34 +24,21 @@ if __name__ == '__main__':
       next(reader)
       for row in reader:
         try:
-          if args.vote_type == 'binary':
-            predictions[int(row[0])].append(int(row[1]))
-          else:
-            predictions[int(row[0])].append(float(row[1]))
+          predictions[int(row[0])].append(int(row[1]))
         except:
-          if args.vote_type == 'binary':
-            predictions[int(row[0])] = [int(row[1])]
-          else:
-            predictions[int(row[0])] = [float(row[1])]
+          predictions[int(row[0])] = [int(row[1])]
 
-  with open(join(args.folder_path, 'vote-%s.csv' % args.vote_type), 'w') as file:
+  with open(join(args.folder_path, 'vote.csv'), 'w') as file:
     writer = csv.writer(file)
     writer.writerow(['QuestionPairID', 'Prediction'])
 
     for example in predictions:
-      if args.vote_type == 'binary':
-        ones = sum(predictions[example])
-        zeros = 5 - ones
+      ones = sum(predictions[example])
+      zeros = 5 - ones
 
-        if ones == zeros:
-          writer.writerow([example, random.randint(0, 1)])
-        elif ones > zeros:
-          writer.writerow([example, 1])
-        else:
-          writer.writerow([example, 0])
+      if ones == zeros:
+        writer.writerow([example, random.randint(0, 1)])
+      elif ones > zeros:
+        writer.writerow([example, 1])
       else:
-        s = sum(predictions[example]) / 5
-        if s >= 0.5:
-          writer.writerow([example, 1])
-        else:
-          writer.writerow([example, 0])
+        writer.writerow([example, 0])
